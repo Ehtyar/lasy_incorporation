d0d237b64d796a95aa31000256360aa5b6f0939f
Ehtyar <edgar-marquardt@gmx.de>
2026-01-13 10:36:51 +0100

    added a new image

--- python_modules/runflfoc_angspec.py
+++ python_modules/runflfoc_angspec.py
@@ -17,14 +17,22 @@ import radialGroupDelay as RGD
 import full_field
 import sys
 
+
+
+lines = []
+def printf(string, filename="flfoc_angspec_out/printout"):
+    lines.append(string+"\n")
+    file = open(filename, "w")
+    file.writelines(lines)
+    file.close()
+
 dim = "xyt"
 if sys.argv[1] == "no":
     do_rgd = False
 else:
     do_rgd = True
     vf = c/100*int(sys.argv[1])
-    print("vf:", vf)
-
+    printf(f"vf: {vf}")
 
 l_w = 10.54e-7
 f0 = 7e-2
@@ -34,8 +42,8 @@ tau = 1.5e-14
 E = 6.2
 des_dt = 1.39e-16 # PIConGPU Standardwert
 w0 = f0 * l_w / w / np.pi
-print("w0 =", w0)
-print("w/w0 =",w/w0)
+printf(f"w0 = {w0}")
+printf(f"w/w0 ={w/w0}")
 if dim == "xyt":
     npoints = (3000, 3000, 3000)
     hi = (2*w, 2*w, 9*tau)
@@ -44,20 +52,20 @@ if dim == "xyt":
 elif dim == "rt":
     p_per_r = 1./3
     picpoints_per_p = 2
-    print("points in file:", int(1024/picpoints_per_p))
+    printf(f"points in file:{int(1024/picpoints_per_p)}")
     spacing = 0.1772e-6 * p_per_r * 3 # PIConGPU Standardwert
     npoints = (int(2*w/spacing), 5000)
     cut_frac = 0.3
     hi = (2*w, 9*tau)
     lo = (0., -15*tau)
     offset_frac = hi[1]/4 / (hi[1]-lo[1])
-    print(offset_frac)
-print(npoints)
+    printf(str(offset_frac))
+printf(str(npoints))
 
-print(np.pi*w0**2/l_w)
-print(100000*des_dt*c)
+printf(str(np.pi*w0**2/l_w))
+printf(str(100000*des_dt*c))
 
-print("time:", (time.time()-start)/60, "min")
+printf(f"time: {(time.time()-start)/60} min")
 
 profile = CombinedLongitudinalTransverseProfile(l_w, (1,0),
     GaussianLongitudinalProfile(l_w, tau, 0),
@@ -69,7 +77,7 @@ propagator = AngularSpectrumPropagator(profile.omega0, "xyt")
 laser = Laser(dim, lo, hi, npoints, profile)
 laser.add_propagator(propagator)
 #laser.show()
-print("time:", (time.time()-start)/60, "min")
+printf(f"time: {(time.time()-start)/60} min")
 
 axiparabola = Axiparabola(f0, delta, 1.7*w)
 
@@ -85,27 +93,26 @@ else:
     def ztime(z):
         r2 = (z-axiparabola.f0) / axiparabola.delta*axiparabola.R**2
         return 1/c*(z+r2/2/z-2*axiparabola.R**2/4/axiparabola.delta*np.log(1+axiparabola.delta/axiparabola.f0*r2/axiparabola.R**2))
-print("time:", (time.time()-start)/60, "min")
+printf(f"time: {(time.time()-start)/60} min")
 
 laser.apply_optics(axiparabola)
 laser.show()
-print("time:", (time.time()-start)/60, "min")
+printf(f"time: {(time.time()-start)/60} min")
 
 laser.propagate(f0)
 laser.show()
-print("time:", (time.time()-start)/60, "min")
+printf(f"time: {(time.time()-start)/60} min")
 
-print("w =", get_w0(laser.grid, laser.dim))
+printf(f"w = {get_w0(laser.grid, laser.dim)}")
 
 #full_field.laser_to_openPMD(laser, "fl_foc_"+sys.argv[1], Nt=1536, Nx=int(1024/picpoints_per_p), Ny=int(1024/picpoints_per_p), conversion_safety=1.1,
 #                            points_between_r=p_per_r, forced_dt=des_dt, offset_frac=1*offset_frac, file_format="bp", data_step=picpoints_per_p)
 
 laser.show()
-print("time:", (time.time()-start)/60, "min")
+printf(f"time: {(time.time()-start)/60} min")
 
-print(laser.grid.npoints)
 tps = full_field.get_tpeak(laser)
-print(tps)
+printf(f"{tps}")
 N = int(sys.argv[2])
 zs = np.zeros(N+1)
 ts = np.zeros(N+1)
@@ -118,16 +125,16 @@ ws[0] = get_w0(laser.grid, laser.dim)
 for n in range(N):
     laser.propagate(delta/N)
     ts[n+1] = full_field.get_tpeak(laser) - tps
-    print("t:", ts[n+1])
+    printf(f"t: {ts[n+1]}")
     tes[n+1] = ztime(f0+(n+1)*delta/N) - (f0+(n+1)*delta/N) / c
-    print("t expect", tes[n+1])
+    printf(f"t expect {tes[n+1]}")
     ws[n+1] = get_w0(laser.grid, laser.dim)
-    print("w:", ws[n+1])
+    printf(f"w: {ws[n+1]}")
     wes[n+1] = l_w*axiparabola.f0/np.pi/axiparabola.R*np.sqrt(axiparabola.delta/((n+1)*delta/N))
-    print("w expect", wes[n+1])
+    printf(f"w expect {wes[n+1]}")
     zs[n+1] = (n+1)*delta/N
-    print("z:", zs[n+1]+f0)
-    print("time:", (time.time()-start)/60, "min")
+    printf(f"z: {zs[n+1]+f0}")
+    printf(f"time: {(time.time()-start)/60} min")
 
 if do_rgd:
     name="flfoc_"+sys.argv[1]
@@ -155,3 +162,5 @@ ax.set_xlabel("$z-f_0$/mm")
 ax.set_ylabel("$w/\\mu$m")
 
 plt.savefig("flfoc_angspec_out/lasy_"+name+"_ws.png")
+printf("done")
+print("done")
