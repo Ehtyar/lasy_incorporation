9eff58a6f81106e0a496c2751194c6e06ad342d7
Edgar Marquardt <edgar-marquardt@gmx.de>
2026-01-16 22:37:05 +0100

    make lib work

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,5 +1,12 @@
-from full_field import *
-from showdata import *
-from axiparabola_theory import *
-from radialGroupDelay import *
-from rectProfile import *
+from .full_field import *
+from .showdata import *
+from .axiparabola_theory import *
+from .radialGroupDelay import *
+from .rectProfile import *
+
+__all__=["RectProfile", "RectTransverseProfile", "RectLongitudinalProfile",
+         "RadialGroupDelay", "tau_D_const_v", "v_plasma", "show_file", "show_w",
+         "plot_w", "show_lpeak", "plot_lpeak", "show_metadata", "show_lwfa",
+         "get_full_field", "get_tpeak", "cfl_condition", "write_to_openpmd_file",
+         "show", "laser_to_openPMD", "memory_calc", "show_field", "Axiparabola_Ambat",
+         "f", "rz", "sf", "tfr", "vfr", "vftf", "rtf", "tfz", "ztf", "wz", "wtf"]
