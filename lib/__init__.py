c52848a7aaa4078475b3bfb9a611b84743438965
Ehtyar <edgar-marquardt@gmx.de>
2026-02-17 13:19:20 +0100

    made lib a better python package

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,15 +1,8 @@
-from .full_field import *
-from .showdata import *
-from .axiparabola_theory import *
-from .radialGroupDelay import *
-from .rectProfile import *
-from .ptime import *
+from . import full_field
+from . import showdata
+from . import axiparabola_theory
+from . import radialGroupDelay
+from . import rectProfile
+from . import ptime
 
-__all__=["RectProfile", "RectTransverseProfile", "RectLongitudinalProfile",
-         "RadialGroupDelay", "tau_D_const_v", "v_plasma", "show_file", "show_w",
-         "plot_w", "show_lpeak", "plot_lpeak", "show_metadata", "show_lwfa",
-         "get_full_field", "get_tpeak", "cfl_condition", "write_to_openpmd_file",
-         "show", "laser_to_openPMD", "memory_calc", "show_field", "Axiparabola_Ambat",
-         "f", "rz", "sf", "tfr", "vfr", "vftf", "rtf", "tfz", "ztf", "wz", "wtf",
-		 "rtime", "ptime", "start_clock", "read_clock", "set_clock", "pause_clock",
-		 "unpause_clock", "print_clock"]
+__all__=["full_field", "ptime", "rectProfile", "radialGroupDelay", "axiparabola_theory", "showdata"]
